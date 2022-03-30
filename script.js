8a3fcd9b8ccf607b617663939b7a51bc555637bf
Sean Scott <seanscott565@gmail.com>
2022-03-29 20:29:57 -0400

    Removed spacing

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ temp.addEventListener('click', function(e) {
     (json => output.innerText = `The tempature is ${json.main.temp} degrees farenheit`))
 })
 
+
+
 humidity.addEventListener('click', function(e) {
     fetch('https://api.openweathermap.org/data/2.5/weather?id=4160021&units=imperial&appid=e743c2a63bc0a7fdd6b2299493d3570a').then
     (response => response.json().then
@@ -26,14 +28,14 @@ feelslike.addEventListener('click', function(e) {
     fetch('https://api.openweathermap.org/data/2.5/weather?id=4160021&units=imperial&appid=e743c2a63bc0a7fdd6b2299493d3570a').then
     (response => response.json().then
     (json => output.innerText = `It feels like ${json.main.feels_like} degrees fahrenheit`))
-})  
+})
 
 maxTemp.addEventListener('click', function(e) {
     output.style.opacity = 1;
     fetch('https://api.openweathermap.org/data/2.5/weather?id=4160021&units=imperial&appid=e743c2a63bc0a7fdd6b2299493d3570a').then
     (response => response.json().then
     (json => output.innerText = `The high is ${json.main.temp_max} degrees fahrenheit`))
-}) 
+})
 
 lowTemp.addEventListener('click', function(e) {
     output.style.opacity = 1;
